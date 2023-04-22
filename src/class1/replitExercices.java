ca8f59b7ce91abb8c6af3d0f740d38f241c65b97
Germonlar <gerardo_mlara@hotmail.com>
2023-04-21 23:33:19 -0600

    first commit

---
+++ src/class1/replitExercices.java
@@ -0,0 +1,40 @@
+package class1;
+
+import java.util.Scanner;
+
+public class replitExercices {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30, 40, 50},
+                {1, 2, 3, 4, 5,},
+                {5, 5, 5, 5, 5},
+                {10, 8, 6, 4, 2}
+    };
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if((arr[i][j] % 2) != 0){
+                    System.out.println(arr[i][j]);
+                }
+            }
+        }
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/class11/ExamplesClasses.java
@@ -0,0 +1,11 @@
+package class11;
+
+public class ExamplesClasses {
+    public static void main(String[] args)
+    {
+    
+
+
+
+    }
+}
